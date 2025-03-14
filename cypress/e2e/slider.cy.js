c51652067489dc78168423c31ed5f74290561ef0
janicka <wjanicka@agh.edu.pl>
2025-03-14 12:59:15 +0100

    Add Gallery Test and Content Test

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,62 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Navigation Test', function () {
+  it('Ensures the user can navigate slides using next and prev buttons', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').invoke('text').then((firstSlideText) => {
+      cy.get('.swiper-button-next').click();
+      cy.wait(1000); 
+      cy.get('.swiper-slide-active').invoke('text').then((nextSlideText) => {
+        expect(nextSlideText).not.to.eq(firstSlideText);
+        cy.get('.swiper-button-prev').click();
+        cy.wait(1000);
+        cy.get('.swiper-slide-active').invoke('text').should('eq', firstSlideText);
+      });
+    });
+  });
+});
+
+describe('Swiper Slide Content Test', function () {
+  it('Verifies that each slide displays the correct title and description', function () {
+    cy.visit('http://localhost:3000');
+    const slides = [
+      { title: 'Rome', description: 'Italy' },
+      { title: 'London', description: 'United Kingdom' },
+      { title: 'Paris', description: 'France' }
+    ];
+    slides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(1000);
+      }
+      cy.get('.swiper-slide-active').should('contain', slide.title);
+      cy.get('.swiper-slide-active').should('contain', slide.description);
+    });
+  });
+});
+
+describe('Gallery on different devices', () => {
+  const devices = [
+    { name: 'Desktop', width: 1280, height: 800 },
+    { name: 'Tablet', width: 768, height: 1024 },
+    { name: 'Phone', width: 375, height: 667 },
+  ];
+
+  devices.forEach((device) => {
+    it(`should work correctly on ${device.name}`, () => {
+      cy.viewport(device.width, device.height);
+      cy.visit('http://localhost:3000');
+      cy.get('.swiper').should('be.visible');
+      cy.get('.swiper-wrapper').should('have.css', 'display', 'flex');
+      cy.get('.swiper-slide').should('be.visible');
+      cy.get('.swiper-button-next').should('be.visible').and('not.be.disabled').click();
+      cy.get('.swiper-button-prev').should('be.visible').and('not.be.disabled').click();
+      cy.get('.swiper-slide-active').should('be.visible');
+      cy.get('.swiper-slide-active').should('have.class', 'swiper-slide-visible');
+      cy.get('.swiper-slide-active').find('.card-description').should('be.visible');
+    });
+  });
+});
+
